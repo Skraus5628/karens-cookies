204443c5509a1ebe84781037af804b214a920d67
Skraus5628 <53798819+Skraus5628@users.noreply.github.com>
2020-01-19 17:12:05 -0600

    set up component skeleton

---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Karen's Cookies</h1>
+            </div>
+        )
+    }
+   
+}
+
